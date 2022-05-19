45e5c6bcf8f1cff50e8fd9adfec6225e9ecd2af7
Ilfat <ilfat95@mail.ru>
2022-05-19 15:16:24 +0500

    Исправления по замечаиям

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -14,13 +14,18 @@ namespace Task2
 
             for (int i = 0; i < ArrayNum.Count(); i++)
             {
-                ArrayNum[i] = rand.Next();
+                ArrayNum[i] = rand.Next(0, 100);
             }
 
             Console.WriteLine(ArrayNum.Max());
             Console.WriteLine(ArrayNum.Min());
             Console.WriteLine(ArrayNum.Sum());
-            Console.WriteLine(ArrayNum.Sum()/(double)ArrayNum.Count());
+            Console.WriteLine(ArrayNum.Sum() / (double)ArrayNum.Count());
+            foreach (int num in ArrayNum)
+            {
+                if (num % 2 == 0)
+                    Console.Write(num + " ");
+            }
 
             Console.ReadLine();
         }
